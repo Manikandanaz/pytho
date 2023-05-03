581cf0e5daabd0641ea944af664e3eeacc27523e
Manikandanaz <Manikandanaz@outlook.com>
2023-05-03 09:03:24 +0530

    ss

--- Intellipaat/PythonBasis03052023/Day01.py
+++ Intellipaat/PythonBasis03052023/Day01.py
@@ -3,3 +3,14 @@
 #its widely used in DS,AI,ML,WebApp etc.
 #Its Interpreted Language
 #its based on Oops concept
+#To execute python : Python -c[command | -m module-name |script|-] [args]
+#All arguments will end up in sys.argv , sys.argv[0] always give source program's name
+##Variables(int,string,float,boolean)
+#rules of Variables(Keywords&Inbuilt Function name cannot be used as variable name)
+#(input()--getting input from user--always it receives input as string)
+#if we want to convert to int for the input() we need to do typecast int(input()),float(input()) etc)
+#Datatypes(1.Basic/universal,2.Collective)
+#basic/universal datatypes--Single value provided to variable(int,float,boolean,str)
+#collective datatypes--(store multiple values in single variable(Mutable/Immutable))
+#Mutable datatype(value of the variable will get overrided)
+#ImMutable datatype(value of the variable wont get overrided)
