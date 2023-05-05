add6772511a3a59b070ee1e5fd5cb1ec7d44d0f8
Manikandanaz <Manikandanaz@outlook.com>
2023-05-05 08:56:18 +0530

    asas

--- Intellipaat/PythonBasis03052023/Day01.py
+++ Intellipaat/PythonBasis03052023/Day01.py
@@ -12,5 +12,53 @@
 #Datatypes(1.Basic/universal,2.Collective)
 #basic/universal datatypes--Single value provided to variable(int,float,boolean,str)
 #collective datatypes--(store multiple values in single variable(Mutable/Immutable))
-#Mutable datatype(value of the variable will get overrided)
-#ImMutable datatype(value of the variable wont get overrided)
+#Mutable datatype(value of the variable will get overrided--tuple,string)
+#ImMutable datatype(value of the variable wont get overrided-list,set,dict)
+
+#dict
+a={"key":"value","key2":"value2"}
+print(type(a))
+b=print(a.items())
+b=print(a.keys())
+b=print(a.values())
+
+#list
+l=[1,2,'mani',3]
+print(l)
+
+#sets --collection of unique items which will remove duplicate data and its unordered and unindexed
+s={1,5,3,3,'mani'}
+print(s)
+s={True,False,1,0}
+print(str(s))
+
+#conditional statement
+#if
+#if else
+#if elif else
+#nested if else
+
+if 5>10:
+    print('yes')
+else:
+    print('no')
+
+if 5>13:
+    print('yes')
+elif 6>2:
+    print('ee')
+else:
+    print('else')
+
+if 20>10:
+    if 6>20:
+        print('the value is yes')
+    elif 6>13:
+        print('the value is no')
+    else:
+        print("get out")    
+else:
+    print("come out")
+#looping statement
+#for
+#while
