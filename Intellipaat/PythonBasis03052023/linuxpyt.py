37bfec6df8d0c2fd5bb6cdeb2fc792ba242e6583
Manikandanaz <Manikandanaz@outlook.com>
2023-05-10 21:45:58 +0530

    gfgfgS

--- Intellipaat/PythonBasis03052023/linuxpyt.py
+++ Intellipaat/PythonBasis03052023/linuxpyt.py
@@ -11,7 +11,13 @@ for line in Lines:
     line=line.decode('ASCII')
     filepath=str(line).rstrip()
     filename=str(line.replace('.json','').rstrip())
-    cmd='\n '+'Set-AzDataFactoryV2Pipeline -DataFactoryName '+'"'+sys.argv[3]+'"'+' -ResourceGroupName '+'"'+sys.argv[2]+'"'+' -Name '+'"'+filename+'"'+' -DefinitionFile '+'"'+filepath+'"'+' -Force'
+    sourcepath= (os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+    if sourcepath.endswith('dataset') == True:
+        cmd='\n '+'Set-AzDataFactoryV2Dataset -DataFactoryName '+'"'+sys.argv[3]+'"'+' -ResourceGroupName '+'"'+sys.argv[2]+'"'+' -Name '+'"'+filename+'"'+' -DefinitionFile '+'"'+filepath+'"'+' -Force'
+    elif sourcepath.endswith('pipeline') == True:
+        cmd='\n '+'Set-AzDataFactoryV2Pipeline -DataFactoryName '+'"'+sys.argv[3]+'"'+' -ResourceGroupName '+'"'+sys.argv[2]+'"'+' -Name '+'"'+filename+'"'+' -DefinitionFile '+'"'+filepath+'"'+' -Force'
+    else:
+        raise Exception("Sorry, Only Dataset and Pipeline can be moved")
     ls=ls+cmd
 print(ls)
 ps_command=ls
