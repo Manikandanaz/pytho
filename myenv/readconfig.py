f07e2206e5075d0363e2ecf1d7017b6129544ed1
learnwithmaniv <learnwithmaniv@outlook.com>
2023-05-16 19:38:09 +0530

    ccc

--- myenv/readconfig.py
+++ myenv/readconfig.py
@@ -4,11 +4,14 @@ import fdl_util as util
 
 
 filedir=os.path.dirname(os.path.realpath(__file__))
+filedir1=os.path.dirname(os.path.abspath(__file__))
+filedir2=os.getcwd()
 conffile=os.path.join(filedir,util.base_basefileconfigname)
 print(filedir)
 print(conffile)
+print(filedir1)
+print(filedir2)
 
 with open(conffile) as configfile:
     confdata = json.load(configfile)
-
 print(confdata['dev']['DB_USER'])
