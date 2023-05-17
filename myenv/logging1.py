e514dcfb6de3aafd75f4c93fe6b4b03fa34c6166
learnwithmaniv <learnwithmaniv@outlook.com>
2023-05-18 01:20:22 +0530

    comm

--- myenv/logging1.py
+++ myenv/logging1.py
@@ -1,26 +1,15 @@
 import logging
+import logging.config
+import os
 class lg:
     def sample(self):
         #create logger
-        logger=logging.getLogger("loggingpyfile")
-        #set level
-        logger.setLevel(logging.DEBUG)
-        #define consolehandler/filehandler
-        ch=logging.StreamHandler()
-        fh=logging.FileHandler("logfile","a")
-        #create formatter
-        formatr=logging.Formatter('%(asctime)s %(filename)s %(levelname)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S,uuu')
-        formatr1=logging.Formatter('{asctime} {created} {filename} {levelname} {message}',datefmt='%Y-%m-%d %H:%M:%S,uuu',style='{')
-        #set formatter
-        ch.setFormatter(formatr)
-        fh.setFormatter(formatr1)
-        #add formatter
-        logger.addHandler(ch)
-        logger.addHandler(fh)
-        logger.debug("debug statements")
-        logger.info("infp statements")
-        logger.warning("war statements")
-        logger.error("err statements")
-        logger.critical("cri statements")
+        logger=os.path.join(os.getcwd(),'dictlog.conf')
+        
+        logging.config.fileConfig(logger)
+        logger=logging.getLogger("mylogger")
+        logger.debug("Unexpected:{}".format(os.getcwd()),extra= {'clientip': '192.168.0.1', 'user': 'fbloggs'})
+       # logger.info("Unexpected:{}".format(os.getcwd()),extra= {'clientip': '192.168.0.1', 'user': 'info'})
+       
 obj=lg()
 obj.sample()
