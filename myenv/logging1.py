fe1e839c5013c40d860d733f85d02963c6219a64
learnwithmaniv <learnwithmaniv@outlook.com>
2023-05-20 23:17:02 +0530

    sync

--- myenv/logging1.py
+++ myenv/logging1.py
@@ -9,7 +9,7 @@ class lg:
         logging.config.fileConfig(logger)
         logger=logging.getLogger("mylogger")
         logger.debug("Unexpected:{}".format(os.getcwd()),extra= {'clientip': '192.168.0.1', 'user': 'fbloggs'})
-       # logger.info("Unexpected:{}".format(os.getcwd()),extra= {'clientip': '192.168.0.1', 'user': 'info'})
+        logger.error("Unexpected:{}".format(os.getcwd()),extra= {'clientip': '192.168.0.1', 'user': 'info'})
        
 obj=lg()
 obj.sample()
