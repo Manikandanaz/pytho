7e1142f05a15d55999dca5afe9841a794dd5cac3
learnwithmaniv <learnwithmaniv@outlook.com>
2023-05-29 23:10:05 +0530

    commit

--- myenv/logging1.py
+++ myenv/logging1.py
@@ -1,13 +1,18 @@
 import logging
 import logging.config
 import os
+from datetime import datetime
 class lg:
     def sample(self):
         #create logger
         logger=os.path.join(os.getcwd(),'dictlog.conf')
-        
+
+     
         logging.config.fileConfig(logger)
+       
         logger=logging.getLogger("mylogger")
+    
+
         logger.debug("Unexpected:{}".format(os.getcwd()),extra= {'clientip': '192.168.0.1', 'user': 'fbloggs'})
         logger.error("Unexpected:{}".format(os.getcwd()),extra= {'clientip': '192.168.0.1', 'user': 'info'})
        
