b25993b7a41ae62630a5c018774d8a8984e2b453
learnwithmaniv <learnwithmaniv@outlook.com>
2023-06-02 01:07:29 +0530

    ssss

---
+++ myenv/sample.sql
@@ -0,0 +1,3 @@
+create table sample1(a int)
+insert into sample1(a)values(1)
+select * from sample1
--- myenv/test.py
+++ myenv/test.py
@@ -4,13 +4,20 @@ class main():
     def __init__(self):
         print(os.getcwd())
         with open('./myenv/sample.sql','r') as sqlfile:
-            print(sqlfile.read())
-            print(pd.drivers())
+            qry=sqlfile.read()
+            #print(pd.drivers())
             try:
                 #SQL Server Native Client 11.0
                 #pd.connect(connstring='Driver:{SQL Server Native Client 11.0};Server=tcp:itsfidev.database.windows.net,1433;Database=sample;Uid=admin123;Pwd=Ramco@1234;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
                 #cnxn = pd.connect('DSN=newdsn;UID=admin123;PWD=Ramco@1234')
-                cnxn = pd.connect('DSN=newdsn;UID=admin123;PWD=Ramco@1234')
+                conn = pd.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                      'SERVER=itsfidev.database.windows.net;'
+                      'DATABASE=sample;UID=admin123;PWD=Ramco@1234;')
+                print(qry)
+                cursor = conn.cursor()
+                with conn:
+                    cursor.execute(qry)
+                    rows = cursor.fetchall()
             except Exception as err:
                 print(err)
 if __name__=='__main__':
