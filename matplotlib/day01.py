249680a37ad87d10d1c0bd741811c841b3bed401
learnwithmaniv <learnwithmaniv@outlook.com>
2023-06-07 09:00:08 +0530

    commir=t

---
+++ matplotlib/day01.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib_inline as inl
+import numpy as np
+a=np.linspace(0,10,20)
+b=np.linspace(0,10,5)
+fig=plt.figure(figsize=(10,5))
+ax1=plt.subplot()
+ax1.plot(a)
+ax1.plot(b)
+plt.show()
