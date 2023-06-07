f6db4d93affea9322a1994a2a729afe7fd915553
learnwithmaniv <learnwithmaniv@outlook.com>
2023-06-07 22:00:05 +0530

    225211

--- matplotlib/day01.py
+++ matplotlib/day01.py
@@ -3,9 +3,13 @@ import matplotlib_inline as inl
 import numpy as np
 a=np.linspace(0,10,20)
 b=np.linspace(0,10,5)
-fig=plt.figure(figsize=(10,5))
+fig=plt.figure(figsize=(4,5))
 ax1=plt.subplot()
 ax1.set_title("DATA VISUALTION")
-ax1.plot(a)
+
+ax1.set_label("aa",)
+
+zz=ax1.plot(a,label='Inline label')
+ax1.legend(handles=zz)
 ax1.plot(b)
 plt.show()
