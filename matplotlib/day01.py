aa6f3b58a78f20ca402f47ce47ceda6eb22bdadb
learnwithmaniv <learnwithmaniv@outlook.com>
2023-06-08 08:03:21 +0530

    ccc

--- matplotlib/day01.py
+++ matplotlib/day01.py
@@ -6,10 +6,7 @@ b=np.linspace(0,10,5)
 fig=plt.figure(figsize=(4,5))
 ax1=plt.subplot()
 ax1.set_title("DATA VISUALTION")
-
 ax1.set_label("aa",)
-
 zz=ax1.plot(a,label='Inline label')
-ax1.legend(handles=zz)
 ax1.plot(b)
 plt.show()
